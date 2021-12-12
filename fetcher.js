4eae029faf5c5b6d23e407f750e32c2705720a35
Yan Sun <tsun812@gmail.com>
2021-12-11 19:25:40 -0800

    added google fetching as example

--- fetcher.js
+++ fetcher.js
@@ -6,10 +6,11 @@ let url = process.argv[2];
 let path = process.argv[3];
 request(url, function (error, response, body) {
   console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('statusCode:',response.statusCode); // Print the response status code if a response was received
+  // Print the HTML for the Google homepage.
+  const res = JSON.stringify(response);
   if(response.statusCode === 200){
-  fs.writeFile(path, response, 'utf8' , (err, response) => {
+  fs.writeFile(path, res, 'utf8' , (err, response) => {
     if (err) {
       return console.error(err)
       
